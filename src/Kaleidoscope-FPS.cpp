0af7531aeabc9af8e22464f9a7b86362a0afbc33
Chris Reinhardt <chris@triv.org>
2017-08-20 19:44:10 -0400

    Color the keys by category.

--- src/Kaleidoscope-FPS.cpp
+++ src/Kaleidoscope-FPS.cpp
@@ -5,7 +5,13 @@
 
 byte FPS_::row = 255, FPS_::col = 255;
 uint8_t FPS_::FPSLayer;
-cRGB FPS_color = CRGB(0, 160, 0);
+
+cRGB basic_movement_color = CRGB(200, 0, 0);
+cRGB movement_modifier_color = CRGB(0, 150, 0);
+cRGB actions_color = CRGB(0, 0, 200);
+cRGB meta_color = CRGB(100, 0, 100);
+cRGB toggle_color = CRGB(140, 70, 0);
+cRGB weapon_color = CRGB(100, 100, 100);
 cRGB off_color = CRGB(0, 0, 0);
 
 FPS_::FPS_(void) {
@@ -21,13 +27,30 @@ void FPS_::loopHook(bool postClear) {
 
   for (uint8_t r = 0; r < ROWS; r++) {
     for (uint8_t c = 0; c < COLS; c++) {
-      Key k = Layer.lookupOnActiveLayer(r, c);
-      Key layer_key = Layer.getKey(FPSLayer, r, c);
+      //Key k = Layer.lookupOnActiveLayer(r, c);
+      //Key layer_key = Layer.getKey(FPSLayer, r, c);
+      Key k = Layer.getKey(FPSLayer, r, c);
 
-      if ((k != layer_key) || (k.flags != KEY_FLAGS)) {
-        LEDControl.setCrgbAt(r, c, off_color);
-      } else {
-        LEDControl.setCrgbAt(r, c, FPS_color);
+      if ((k == Key_FPS_fwd) || (k == Key_FPS_bkwds) || (k == Key_FPS_left) || (k == Key_FPS_right)) {
+          LEDControl.setCrgbAt(r, c, basic_movement_color);
+      }
+      else if ((k == Key_FPS_crouch) || (k == Key_FPS_run) || (k == Key_FPS_jump)|| (k == Key_FPS_walk)) {
+          LEDControl.setCrgbAt(r, c, movement_modifier_color);
+      }
+      else if ((k == Key_FPS_use1) || (k == Key_FPS_use2) || (k == Key_FPS_use3) || (k == Key_FPS_reload) || (k == Key_FPS_zoom)) {
+          LEDControl.setCrgbAt(r, c, actions_color);
+      }
+      else if ((k == Key_FPS_console) || (k == Key_FPS_board)) {
+          LEDControl.setCrgbAt(r, c, meta_color);
+      }
+      else if ((k == Key_ToggleFPS)) {
+          LEDControl.setCrgbAt(r, c, toggle_color);
+      }
+      else if ((Key_1 <= k) && (k <= Key_0)) {
+          LEDControl.setCrgbAt(r, c, weapon_color);
+      }
+      else {
+          LEDControl.setCrgbAt(r, c, off_color);
       }
     }
   }
